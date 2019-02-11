ed5ec973b4773d7c7f1c7d0ee53d6d274afc57ec
Lauren Winter <laurenw@Laurens-MacBook-Pro.local>
2019-02-11 11:21:42 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,22 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if (cardsInPlay.length[0] === cardsInPlay[2]) {
-	alert("You found a match");
-} else {
-	alert("Sorry try again")
+var flipCard = function (cardId) {
+console.log("User Flipped" + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length ===2) {
+	checkForMatch();
 }
+};
+
+
+flipCard(0);
+flipCard(2);
